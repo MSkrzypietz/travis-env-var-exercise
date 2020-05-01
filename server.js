66da4638a4e5ef288477d42b4f53a6c9713cd0a4
Michael Skrzypietz <michael.skr97@gmail.com>
2020-05-01 12:09:08 +0200

    add log using a process env variable

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ function sleep(ms) {
 async function main() {
   while(true) {
     console.log("Microservices rock!");
+    console.log(`My favorite food is ${process.env.FAVORITE_FOOD}`);
     await sleep(5000);
   }
 }
